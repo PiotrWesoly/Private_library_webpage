8304b9e461b1d41ec236fea8afdeb7aa8df34327
PiotrWesoly <Piotrpwesoly@gmail.com>
2021-12-25 13:39:31 +0100

    Add user database and login check
    Add user database and login check
    
    if user is in database, it will forward him to the welcome page, if he is not, he has to register

--- dataBase/start.php
+++ dataBase/start.php
@@ -14,31 +14,31 @@
 <body>
 <?php
 
-require('config.php');
+require('../config.php');
 
 $baza = mysqli_connect($hostname, $username, $password, $db);
 
-$select = 'alter table zwierzaki add constraint nazwa_konstrejnta foreign key (ID_kolor) references kolor(ID) on update cascade on delete set null;';
+//$select = 'alter table zwierzaki add constraint nazwa_konstrejnta foreign key (ID_kolor) references kolor(ID) on update cascade on delete set null;';
 
-/*
-	constraint nazwa_konstrejnta foreign key ID_kolor references kolor(ID) on update cascade on delete set null
 
-$select = 'create table kolor 
-(
-	id serial PRIMARY KEY,
-	nazwa varchar(30) NOT NULL unique
+// 	constraint nazwa_konstrejnta foreign key ID_kolor references kolor(ID) on update cascade on delete set null
+
+// $select = 'create table kolor 
+// (
+// 	id serial PRIMARY KEY,
+// 	nazwa varchar(30) NOT NULL unique
+// );';
+
+$select = 'CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );';
-
-$select = 'create table zwierzaki 
-(
-	id serial PRIMARY KEY,
-	nazwa VARCHAR(30) unique not NULL,
-	wiek int, 
-	ilosc_nog int
-);';*/
 
 $result = mysqli_query($baza, $select)
 			or die(mysqli_error($baza));
+
 ?>
 </body>
 </html>
